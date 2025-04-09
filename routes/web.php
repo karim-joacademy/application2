aaf25b699c7656275a3412b9324e9012cf9fd1b8
karim-joacademy <karim.joacademy@gmail.com>
2025-04-09 09:47:27 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Routee::get('/setup', function () {
+    $creds = [
+        'email' => 'admin@admin.com',
+        'password' => 'karim',
+    ];
+
+    if(!Auth::attempt($creds)) {
+        $user = new User();
+
+        $user->name = 'admin';
+        $user->email = $creds['email'];
+        $user->password = Hash::make($creds['password']);
+
+        $user->save();
+    }
 });
